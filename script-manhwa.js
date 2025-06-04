a61292d095b388dfedaabf1a456224ef402dc0d4
MuchoM158 <jhonatan.escenify@gmail.com>
2025-06-03 21:30:18 -0500

    Update script-manhwa.js

--- script-manhwa.js
+++ script-manhwa.js
@@ -1,15 +1,26 @@
 function mostrarCapitulos(manhwa) {
     const capitulos = {
-        'manhwa1': ['SNV cap 01.pdf', 'SNV cap 02.pdf', 'SNV cap 03.pdf', 'SNV cap 04.pdf'],
-        'manhwa2': ['Vagabond cap 01.pdf', 'Vagabond cap 02.pdf']
+        'manhwa1': [
+            { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/1KMs4DBuoagsiE5LaZh_gGDxPZflgcbHC/preview" },
+            { nombre: "Capítulo 2", url: "https://drive.google.com/file/d/1bcnCIFYs3jc2_iDzqVI3wKA1tHtxytsL/preview" },
+            { nombre: "Capítulo 3", url: "https://drive.google.com/file/d/1qOsPftdxz4p2BPAetZ2Z-7LWuzBeHyWY/preview" },
+            { nombre: "Capítulo 4", url: "https://drive.google.com/file/d/1iHIlrnhrvqxeW-BFt-e-6BLIyD48iiJH/preview" }
+        ],
+        'manhwa2': [
+            { nombre: "Capítulo 1", url: "Vagabond cap 01.pdf" },
+            { nombre: "Capítulo 2", url: "Vagabond cap 02.pdf" },
+            { nombre: "Capítulo 3", url: "Vagabond cap 02.pdf" },
+            { nombre: "Capítulo 4", url: "Vagabond cap 02.pdf" }
+        ]
     };
+
     const lista = document.getElementById("lista-capitulos");
     lista.innerHTML = "";
     
-    capitulos[manhwa].forEach((archivo, index) => {
+    capitulos[manhwa].forEach(capitulo => {
         let item = document.createElement("p");
-        item.textContent = `Capítulo ${index + 1}`;
-        item.onclick = () => mostrarPDF(archivo, manhwa);
+        item.textContent = capitulo.nombre;
+        item.onclick = () => mostrarPDF(capitulo.url, manhwa);
         lista.appendChild(item);
     });
 
@@ -23,7 +34,6 @@ function cerrarCapitulos() {
 function mostrarPDF(pdfUrl, manhwa) {
     document.getElementById("visor-pdf").src = pdfUrl;
     document.getElementById("visor-container").style.display = "block";
-
     document.getElementById("menu-inicio").style.display = "none";
     document.getElementById("capitulos-container").style.display = "none";
 }
