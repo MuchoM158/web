9d3e2587cb16b151a6f9147f14c8f0eee595d2c0
MuchoM158 <jhonatan.escenify@gmail.com>
2025-06-04 00:11:07 -0500

    Update script-manga.js

--- script-manga.js
+++ script-manga.js
@@ -2,13 +2,15 @@ function mostrarCapitulos(manga) {
     const capitulos = {
         'manga1': [
             { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/1KMs4DBuoagsiE5LaZh_gGDxPZflgcbHC/preview" },
-            { nombre: "Capítulo 2", url: "SNV cap 02.pdf" },
-            { nombre: "Capítulo 3", url: "SNV cap 03.pdf" },
-            { nombre: "Capítulo 4", url: "SNV cap 04.pdf" }
+            { nombre: "Capítulo 2", url: "https://drive.google.com/file/d/1bcnCIFYs3jc2_iDzqVI3wKA1tHtxytsL/preview" },
+            { nombre: "Capítulo 3", url: "https://drive.google.com/file/d/1qOsPftdxz4p2BPAetZ2Z-7LWuzBeHyWY/preview" },
+            { nombre: "Capítulo 4", url: "https://drive.google.com/file/d/1iHIlrnhrvqxeW-BFt-e-6BLIyD48iiJH/preview" }
         ],
         'manga2': [
-            { nombre: "Vagabond - Capítulo 1", url: "Vagabond cap 01.pdf" },
-            { nombre: "Vagabond - Capítulo 2", url: "Vagabond cap 02.pdf" }
+            { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/1Eiv9_EynV17Fl9TUk02XJgU1dvFw-RMI/preview" },
+            { nombre: "Capítulo 2", url: "https://drive.google.com/file/d/1xUHhYW7RhGrLIKHc3jY46hgVYrKXuz_w/preview" },
+            { nombre: "Capítulo 3", url: "https://drive.google.com/file/d/1ejjDB-17QgW7RMw0KO7JLQiAIpU8hCVc/preview" },
+            { nombre: "Capítulo 4", url: "https://drive.google.com/file/d/1v9WhpTBjdYOVBH3DNWYShttmUVS2q6vJ/preview" }
         ]
     };
 
