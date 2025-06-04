a1d3d0a725af82ccdbd05217b58f393f5f9e85af
MuchoM158 <jhonatan.escenify@gmail.com>
2025-06-04 01:59:51 -0500

    Update script-manhwa.js

--- script-manhwa.js
+++ script-manhwa.js
@@ -1,16 +1,16 @@
 function mostrarCapitulos(manhwa) {
     const capitulos = {
         'manhwa1': [
-            { nombre: "Capítulo 1", url: "https://www.webtoons.com/es/fantasy/omniscient-reader/ep-0-pr%C3%B3logo/viewer?title_no=2208&episode_no=1" },
-            { nombre: "Capítulo 2", url: "https://drive.google.com/file/d/1bcnCIFYs3jc2_iDzqVI3wKA1tHtxytsL/preview" },
-            { nombre: "Capítulo 3", url: "https://drive.google.com/file/d/1qOsPftdxz4p2BPAetZ2Z-7LWuzBeHyWY/preview" },
-            { nombre: "Capítulo 4", url: "https://drive.google.com/file/d/1iHIlrnhrvqxeW-BFt-e-6BLIyD48iiJH/preview" }
+            { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/1TXW_dxefapJM_ZotqGVEp4hEd-K1LEkz/preview" },
+            { nombre: "Capítulo 2", url: "" },
+            { nombre: "Capítulo 3", url: "" },
+            { nombre: "Capítulo 4", url: "" }
         ],
         'manhwa2': [
-            { nombre: "Capítulo 1", url: "Vagabond cap 01.pdf" },
-            { nombre: "Capítulo 2", url: "Vagabond cap 02.pdf" },
-            { nombre: "Capítulo 3", url: "Vagabond cap 02.pdf" },
-            { nombre: "Capítulo 4", url: "Vagabond cap 02.pdf" }
+            { nombre: "Capítulo 1", url: "" },
+            { nombre: "Capítulo 2", url: "" },
+            { nombre: "Capítulo 3", url: "" },
+            { nombre: "Capítulo 4", url: "" }
         ]
     };
 
