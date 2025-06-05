3328badfdc85f63bb05ca5cf3012e705cd4db88f
MuchoM158 <jhonatan.escenify@gmail.com>
2025-06-05 14:49:39 -0500

    Update script-manhwa.js

--- script-manhwa.js
+++ script-manhwa.js
@@ -2,7 +2,7 @@ function mostrarCapitulos(manhwa) {
     const capitulos = {
         'manhwa1': [
             { nombre: "Prólogo", url: "https://drive.google.com/file/d/1TXW_dxefapJM_ZotqGVEp4hEd-K1LEkz/preview" },
-            { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/130DU0-jwOvPWflPEYkxkzYTaRRanTk4x/preview" },
+            { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/1A52qPU4tkNRYR73gfmGthywRJNCKVSbu/preview" },
             { nombre: "Capítulo 2", url: "https://drive.google.com/file/d/19WJk698xKAY9t1Yxl_1jVu65f_gBARIT/preview" },
             { nombre: "Capítulo 3", url: "https://drive.google.com/file/d/19EXkH6LNOmsLlpZILg2fkfVgpeqRpSQH/preview" }
         ],
