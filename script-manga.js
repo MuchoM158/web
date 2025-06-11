2222c6fb6a7e4addc0c179db25061aadb864a1b6
MuchoM158 <jhonatan.escenify@gmail.com>
2025-06-11 00:59:16 -0500

    Update script-manga.js

--- script-manga.js
+++ script-manga.js
@@ -1,10 +1,10 @@
 function mostrarCapitulos(manga) {
     const capitulos = {
         'manga1': [
-            { nombre: "Capítulo 4", url: "https://drive.google.com/file/d/1EJQdalC9CXHbxvALUBth2HnN0kdmbMN5/preview" },
             { nombre: "Capítulo 5", url: "https://drive.google.com/file/d/1HwuEVmzQZxdPkSeOEOpNvifg8x-VMfnI/preview" },
             { nombre: "Capítulo 6", url: "https://drive.google.com/file/d/1NY4RrdwqKv8j1y_qmMUujIyfYj5hNwAH/preview" },
-            { nombre: "Capítulo 7", url: "https://drive.google.com/file/d/18nUJmaoAF4C1JguGvXN6iZf_qLA5wK9m/preview" }
+            { nombre: "Capítulo 7", url: "https://drive.google.com/file/d/18nUJmaoAF4C1JguGvXN6iZf_qLA5wK9m/preview" },
+            { nombre: "Capítulo 8", url: "https://drive.google.com/file/d/1qQKE3E8h2lxARQW_CkycKZf1URCfU3_4/preview" }
         ],
         'manga2': [
             { nombre: "Capítulo 1", url: "https://drive.google.com/file/d/1Eiv9_EynV17Fl9TUk02XJgU1dvFw-RMI/preview" },
